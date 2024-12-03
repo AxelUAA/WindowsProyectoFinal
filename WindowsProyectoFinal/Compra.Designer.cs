bf79a88a01507edfb9baec0d74487553d2996f0b
Omar Romero Lopez <omar.romero.lopez99@gmail.com>
2024-12-03 13:34:18 -0600

    Agregue boton pagar

--- WindowsProyectoFinal/Compra.Designer.cs
+++ WindowsProyectoFinal/Compra.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Algerian", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(618, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Pagar";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Compra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Compra";
             this.Text = "Compra";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
